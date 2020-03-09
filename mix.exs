893f0dac7255354fd6dffd88601add2dd2b3c1ff
Abraão Rossetto <abraaomirandar@gmail.com>
2020-03-09 14:51:34 -0300

    Add CreateAdmin module

--- mix.exs
+++ mix.exs
@@ -41,6 +41,7 @@ defmodule Voting.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
+      {:bcrypt_elixir, "~> 2.0"},
       {:credo, "~> 1.2", only: [:dev, :test], runtime: false}
     ]
   end
